5e9b14b1bf7e8e8c3ef23cd3e2768affdfae5c02
Jing Zhi <e0773051@u.nus.edu>
2025-03-31 04:24:13 +0800

    Update app.tsx

--- dapp/src/pages/_app.tsx
+++ dapp/src/pages/_app.tsx
@@ -2,29 +2,35 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Web3Provider from "../components/Web3Provider";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer"; // Updated to use the custom Footer component
-import "../styles/global.css";
+import Footer from "../components/Footer";
 import { Layout } from "antd";
+import "../styles/global.css";
 
 const { Content } = Layout;
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
+  const siteTitle = "AutoInsure | Decentralized Flight Insurance";
+  const siteDescription = "Decentralized flight delay insurance powered by blockchain and Chainlink oracles";
+
   return (
     <Web3Provider>
       <Head>
-        <title>AutoInsure | Decentralized Flight Insurance</title>
-        <meta name="description" content="Decentralized flight delay insurance powered by blockchain and Chainlink oracles" />
+        <title>{siteTitle}</title>
+        <meta name="description" content={siteDescription} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+
       <Layout style={{ minHeight: "100vh" }}>
         <Navbar />
+
         <Content style={{ padding: "20px 50px", marginTop: "64px" }}>
           <Component {...pageProps} />
         </Content>
+
         <Footer />
       </Layout>
     </Web3Provider>
   );
 }
 
-export default MyApp;
+export default App;
