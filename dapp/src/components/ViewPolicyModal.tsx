434e53bac71e3312b6564e43b5e4144d6e74d158
Jing Zhi <e0773051@u.nus.edu>
2025-04-18 02:20:27 +0800

    Add claim button

--- dapp/src/components/ViewPolicyModal.tsx
+++ dapp/src/components/ViewPolicyModal.tsx
@@ -1,108 +1,93 @@
-import { BaggageUserPolicy } from "@/types/BaggagePolicy";
-import { FlightUserPolicy } from "@/types/FlightPolicy";
-import { getStatusTag } from "@/utils/utils";
-import { Card, Modal, Spin } from "antd";
+import { Card, Modal, Spin, Tag, Button } from "antd";
+import { FlightPolicyStatus, FlightUserPolicy } from "../types/FlightPolicy";
+import { BaggagePolicyStatus, BaggageUserPolicy } from "../types/BaggagePolicy";
 
 type ViewPolicyModalProps = {
   type: "flight" | "baggage";
   policy: FlightUserPolicy | BaggageUserPolicy | undefined;
   onCancel: () => void;
+  onClaim: () => void;
 };
 
-const ViewPolicyModal = ({ type, policy, onCancel }: ViewPolicyModalProps) => {
-  return (
+const ViewPolicyModal = ({ type, policy, onCancel , onClaim }: ViewPolicyModalProps) => {
+  const getStatusTag = (status: FlightPolicyStatus | BaggagePolicyStatus) => {
+    switch (status) {
+      case FlightPolicyStatus.Active | BaggagePolicyStatus.Active:
+        return <Tag color="green">Active</Tag>;
+      case FlightPolicyStatus.Claimed | BaggagePolicyStatus.Claimed:
+        return <Tag color="blue">Claimed</Tag>;
+      case FlightPolicyStatus.Expired | BaggagePolicyStatus.Expired:
+        return <Tag color="orange">Expired</Tag>;
+      default:
+        return <Tag color="gray">Unknown</Tag>;
+    }
+  };
+
+  // Render flight policy details
+  const renderFlightPolicyDetails = (policy: FlightUserPolicy) => (
     <>
-      {/* Optional Modal to show more details (can link to ViewPolicy component) */}
-      <Modal title={`${policy?.template.name}`} open={!!policy} onCancel={onCancel} footer={null} destroyOnClose>
-        <Card>
-          {policy ? (
-            type === "flight" ? (
-              <div>
-                <p>
-                  <strong>Policy ID:</strong> {(policy as FlightUserPolicy).policyId}
-                </p>
-                <p>
-                  <strong>Flight:</strong> {(policy as FlightUserPolicy).flightNumber}
-                </p>
-                <p>
-                  <strong>From:</strong> {(policy as FlightUserPolicy).departureAirportCode}
-                </p>
-                <p>
-                  <strong>To:</strong> {(policy as FlightUserPolicy).arrivalAirportCode}
-                </p>
-                <p>
-                  <strong>Departure Time:</strong> {new Date((policy as FlightUserPolicy).departureTime * 1000).toLocaleString()}
-                </p>
-                <p>
-                  <strong>Date of Purchase:</strong> {new Date((policy as FlightUserPolicy).createdAt * 1000).toLocaleString()}
-                </p>
-                <p>
-                  <strong>Policy Premium:</strong> {(policy as FlightUserPolicy).template.premium} ETH
-                </p>
-                <p>
-                  <strong>Policy Description:</strong> {(policy as FlightUserPolicy).template.description}
-                </p>
-                <p>
-                  <strong>Policy Max Total Payout:</strong> {(policy as FlightUserPolicy).template.maxTotalPayout} ETH
-                </p>
-                <p>
-                  <strong>Policy Payout Per Hour:</strong> {(policy as FlightUserPolicy).template.payoutPerHour} ETH/hr
-                </p>
-                <p>
-                  <strong>Policy Delay Threshold:</strong> {(policy as FlightUserPolicy).template.delayThresholdHours} hours
-                </p>
-                <p>
-                  <strong>Policy Coverage Duration:</strong> {(policy as FlightUserPolicy).template.coverageDurationDays} hours
-                </p>
-                <p>
-                  <strong>Status:</strong> {getStatusTag((policy as FlightUserPolicy).status)}
-                </p>
-              </div>
-            ) : (
-              type === "baggage" && (
-                <div>
-                  <p>
-                    <strong>Policy ID:</strong> {(policy as BaggageUserPolicy).policyId}
-                  </p>
-                  <p>
-                    <strong>Date of Purchase:</strong> {new Date((policy as BaggageUserPolicy).createdAt * 1000).toLocaleString()}
-                  </p>
-                  <p>
-                    <strong>Item Description:</strong> {(policy as BaggageUserPolicy).itemDescription}
-                  </p>
-                  <p>
-                    <strong>Payout to Date:</strong> {(policy as BaggageUserPolicy).payoutToDate} ETH
-                  </p>
-                  <p>
-                    <strong>Policy Premium:</strong> {(policy as BaggageUserPolicy).template.premium} ETH
-                  </p>
-                  <p>
-                    <strong>Policy Description:</strong> {(policy as BaggageUserPolicy).template.description}
-                  </p>
-                  <p>
-                    <strong>Policy Max Total Payout:</strong> {(policy as BaggageUserPolicy).template.maxTotalPayout} ETH
-                  </p>
-                  <p>
-                    <strong>Policy payout if delayed:</strong> {(policy as BaggageUserPolicy).template.payoutIfDelayed} ETH
-                  </p>
-                  <p>
-                    <strong>Policy payout if lost:</strong> {(policy as BaggageUserPolicy).template.payoutIfLost} ETH
-                  </p>
-                  <p>
-                    <strong>Policy coverage duration:</strong> {(policy as BaggageUserPolicy).template.coverageDurationDays} days
-                  </p>
-                  <p>
-                    <strong>Status:</strong> {getStatusTag((policy as BaggageUserPolicy).status)}
-                  </p>
-                </div>
-              )
-            )
-          ) : (
-            <Spin />
-          )}
-        </Card>
-      </Modal>
+      <p><strong>Policy ID:</strong> {policy.policyId}</p>
+      <p><strong>Flight Number:</strong> {policy.flightNumber}</p>
+      <p><strong>From:</strong> {policy.departureAirportCode}</p>
+      <p><strong>To:</strong> {policy.arrivalAirportCode}</p>
+      <p><strong>Departure Time:</strong> {new Date(policy.departureTime * 1000).toLocaleString()}</p>
+      <p><strong>Date of Purchase:</strong> {new Date(policy.createdAt * 1000).toLocaleString()}</p>
+      <p><strong>Payout To Date:</strong> {policy.payoutToDate} ETH</p>
+      <p><strong>Buyer:</strong> {policy.buyer}</p>
+
+      <p><strong>Policy Description:</strong> {policy.template.description}</p>
+      <p><strong>Policy Premium:</strong> {policy.template.premium} ETH</p>
+      <p><strong>Policy Payout Per Hour:</strong> {policy.template.payoutPerHour} ETH/hr</p>
+      <p><strong>Policy Delay Threshold:</strong> {policy.template.delayThresholdHours} hours</p>
+      <p><strong>Policy Max Total Payout:</strong> {policy.template.maxTotalPayout} ETH</p>
+      <p><strong>Policy Coverage Duration:</strong> {policy.template.coverageDurationDays} hours</p>
+      <p><strong>Status:</strong> {getStatusTag(policy.status)}</p>
     </>
+  );
+
+  // Render baggage policy details
+  const renderBaggagePolicyDetails = (policy: BaggageUserPolicy) => (
+    <>
+      <p><strong>Policy ID:</strong> {policy.policyId}</p>
+      <p><strong>Item Description:</strong> {policy.itemDescription}</p>
+      <p><strong>Date of Purchase:</strong> {new Date(policy.createdAt * 1000).toLocaleString()}</p>
+      <p><strong>Payout To Date:</strong> {policy.payoutToDate} ETH</p>
+      <p><strong>Buyer:</strong> {policy.buyer}</p>
+
+      <p><strong>Policy Description:</strong> {policy.template.description}</p>
+      <p><strong>Policy Premium:</strong> {policy.template.premium} ETH</p>
+      <p><strong>Policy Payout If Delayed:</strong> {policy.template.payoutIfDelayed} ETH</p>
+      <p><strong>Policy Payout If Lost:</strong> {policy.template.payoutIfLost} ETH</p>
+      <p><strong>Policy Max Total Payout:</strong> {policy.template.maxTotalPayout} ETH</p>
+      <p><strong>Policy Coverage Duration:</strong> {policy.template.coverageDurationDays} days</p>
+      <p><strong>Status:</strong> {getStatusTag(policy.status)}</p>
+    </>
+  );
+
+  const isClaimable = policy?.status === FlightPolicyStatus.Active || policy?.status === BaggagePolicyStatus.Active;
+
+  return (
+    <Modal title={`${policy?.template.name}`} open={!!policy} onCancel={onCancel} footer={null} destroyOnClose>
+      <Card>
+        {policy ? (
+          type === "flight" ? renderFlightPolicyDetails(policy as FlightUserPolicy) : renderBaggagePolicyDetails(policy as BaggageUserPolicy)
+        ) : (
+          <Spin />
+        )}
+      </Card>
+
+      {isClaimable && (
+        <div style={{ textAlign: "center" }}>
+          <Button
+            type="primary"
+            onClick={onClaim}
+            style={{ marginTop: 16 }}
+          >
+            Claim Policy
+          </Button>
+        </div>
+      )}
+    </Modal>
   );
 };
 
