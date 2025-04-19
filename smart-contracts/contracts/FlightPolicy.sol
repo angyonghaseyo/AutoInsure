4b9e23d92f827cc50f43bef6be6a739fef159832
Jing Zhi <e0773051@u.nus.edu>
2025-04-19 20:55:44 +0800

    Remove airport codes, add modifier to insurer functions, remove markPolicyAsExpired, redesign claimPayout to emit event for evaluation

--- smart-contracts/contracts/FlightPolicy.sol
+++ smart-contracts/contracts/FlightPolicy.sol
@@ -5,8 +5,6 @@ import "./OracleConnector.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-import "hardhat/console.sol";
-
 contract FlightPolicy is ReentrancyGuard {
     address public immutable insurerAddress;
     OracleConnector public oracleConnector;
@@ -39,9 +37,9 @@ contract FlightPolicy is ReentrancyGuard {
         uint256 updatedAt;              // Timestamp of last update
         uint256 premium;                // Cost of the policy (in wei)
         uint256 payoutPerHour;          // Payout per hour of delay (in wei)
-        uint256 delayThresholdHours;    // Minimum delay required (in hours)
         uint256 maxTotalPayout;         // Maximum payout for this policy (in wei)
-        uint256 coverageDurationSeconds;   // How long the policy is valid after purchase
+        uint256 delayThresholdHours;    // Minimum delay required (in hours)
+        uint256 coverageDurationSeconds;// How long the policy is valid after purchase (in seconds)
         PolicyTemplateStatus status;    // Whether the template is active or deactivated
     }
 
@@ -55,8 +53,6 @@ contract FlightPolicy is ReentrancyGuard {
         uint256 policyId;               // Unique ID of the buyer's policy
         PolicyTemplate template;        // Policy template details
         string flightNumber;            // Airline flight number (e.g., "SQ322")
-        string departureAirportCode;    // IATA code (e.g., "SIN")
-        string arrivalAirportCode;      // IATA code (e.g., "LHR")
         uint256 departureTime;          // Scheduled departure timestamp (UTC)
         uint256 createdAt;              // Policy purchase timestamp
         uint256 payoutToDate;           // Total paid out so far (in wei)
@@ -64,17 +60,24 @@ contract FlightPolicy is ReentrancyGuard {
         PolicyStatus status;            // Active, Expired, or Claimed
     }
 
+    enum PayoutResult {
+        Pending, 
+        NotDelayed,
+        NoPayoutDue,
+        Success
+    }
+
     // Buyer policy storage
     mapping(uint256 => UserPolicy) public userPolicies;
     mapping(address => uint256[]) public userPolicyIds;
     uint256 public nextUserPolicyId;
 
-    // Emitted once a payout is really sent
-    event PayoutPending(uint256 indexed policyId, address indexed buyer);
+    // ==================== Events ====================
+    event PayoutEvaluated(uint256 policyId, address buyer, uint256 payout, PayoutResult result);
 
     // ====== Insurer Functions ======
-    // View all purchased policies
-    function getAllPolicies(uint256 currentTime) external view returns (UserPolicy[] memory) {
+    // View all purchased policies (InsurerPolicyTemplates, InsurerClaimsOverview)
+    function getAllPolicies(uint256 currentTime) external view onlyInsurer returns (UserPolicy[] memory) {
         uint256 count = nextUserPolicyId;
         UserPolicy[] memory results = new UserPolicy[](count);
         for (uint256 i = 0; i < count; i++) {
@@ -82,59 +85,9 @@ contract FlightPolicy is ReentrancyGuard {
         }
         return updateStatus(results, currentTime);
     }
-    
-    // TODO: Cron job to mark policies as expired
-    function markPolicyAsExpired(uint256 policyId) external onlyInsurer {
-        require(policyId < nextUserPolicyId, "Invalid policyId");
-        
-        UserPolicy storage policy = userPolicies[policyId];
-        require(policy.status == PolicyStatus.Active, "Policy is not active");
 
-        uint256 expiryTime = policy.createdAt + policy.template.coverageDurationSeconds;
-
-        require(block.timestamp > expiryTime, "Policy has not expired yet");
-
-        policy.status = PolicyStatus.Expired;
-    }
-
-    // ====== User Functions ======
-    // Purchase a policy based on a template
-    function purchasePolicy(PolicyTemplate memory template, string memory flightNumber, string memory departureAirportCode, string memory arrivalAirportCode, uint256 departureTime, address buyer) external returns (uint256) {
-        require(template.status == PolicyTemplateStatus.Active, "Policy template is not active");
-        // require(departureTime > block.timestamp, "Departure time must be in the future");
-        // require(msg.value >= template.premium, "Insufficient premium sent");
-
-        uint256 policyId = nextUserPolicyId++;
-        userPolicies[policyId] = UserPolicy({
-            policyId: policyId,
-            template: template,
-            flightNumber: flightNumber,
-            departureAirportCode: departureAirportCode,
-            arrivalAirportCode: arrivalAirportCode,
-            departureTime: departureTime,
-            createdAt: block.timestamp,
-            payoutToDate: 0,
-            buyer: buyer,
-            status: PolicyStatus.Active
-        });
-        console.log("template: ", template.coverageDurationSeconds);
-
-        userPolicyIds[buyer].push(policyId);
-        return policyId;
-    }
-
-    // Get all policies owned by a user
-    function getUserPolicies(address user, uint256 currentTime) external view returns (UserPolicy[] memory) {
-        uint256 count = userPolicyIds[user].length;
-        UserPolicy[] memory results = new UserPolicy[](count);
-        for (uint256 i = 0; i < count; i++) {
-            results[i] = userPolicies[userPolicyIds[user][i]];
-        }
-        return updateStatus(results, currentTime);
-    }
-
-    // Get all policies by template ID
-    function getUserPoliciesByTemplate(string memory templateId, uint256 currentTime) external view returns (UserPolicy[] memory) {
+    // Get all policies by template ID (PolicyTemplateDrawer)
+    function getUserPoliciesByTemplate(string memory templateId, uint256 currentTime) external view onlyInsurer returns (UserPolicy[] memory) {
         uint256 count = 0;
         for (uint256 i = 0; i < nextUserPolicyId; i++) {
             if (keccak256(abi.encodePacked(userPolicies[i].template.templateId)) == keccak256(abi.encodePacked(templateId))) {
@@ -153,7 +106,40 @@ contract FlightPolicy is ReentrancyGuard {
         return updateStatus(result, currentTime);
     }
 
-    // Claim a policy and give payout based on flight delay
+    // ====== User Functions ======
+    // Purchase a policy based on a template (PurchasePolicy)
+    function purchasePolicy(PolicyTemplate memory template, string memory flightNumber, uint256 departureTime, address buyer) external returns (uint256) {
+        require(template.status == PolicyTemplateStatus.Active, "Policy template is not active");
+        // require(departureTime > block.timestamp, "Departure time must be in the future");
+        // require(msg.value >= template.premium, "Insufficient premium sent");
+
+        uint256 policyId = nextUserPolicyId++;
+        userPolicies[policyId] = UserPolicy({
+            policyId: policyId,
+            template: template,
+            flightNumber: flightNumber,
+            departureTime: departureTime,
+            createdAt: block.timestamp,
+            payoutToDate: 0,
+            buyer: buyer,
+            status: PolicyStatus.Active
+        });
+
+        userPolicyIds[buyer].push(policyId);
+        return policyId;
+    }
+
+    // Get all policies owned by a user (MyPolicies, MyClaims)
+    function getUserPolicies(address user, uint256 currentTime) external view returns (UserPolicy[] memory) {
+        uint256 count = userPolicyIds[user].length;
+        UserPolicy[] memory results = new UserPolicy[](count);
+        for (uint256 i = 0; i < count; i++) {
+            results[i] = userPolicies[userPolicyIds[user][i]];
+        }
+        return updateStatus(results, currentTime);
+    }
+
+    // Claim a policy and give payout based on flight delay (ViewPolicyModal)
     function claimPayout(uint256 policyId, address buyer) external nonReentrant returns (uint256, UserPolicy memory) {
         require(policyId < nextUserPolicyId, "Invalid policyId");
         UserPolicy storage policy = userPolicies[policyId];   
@@ -162,23 +148,42 @@ contract FlightPolicy is ReentrancyGuard {
 
         (bool dataReceived, bool isDelayed, uint256 delayHours) = oracleConnector.getFlightStatus(policy.flightNumber, Strings.toString(policy.departureTime));
 
+        // Retry mechanism
         if (!dataReceived) {
-            emit PayoutPending(policyId, buyer);
+            emit PayoutEvaluated(policyId, buyer, 0, PayoutResult.Pending);
             return (0, policy);
         }
 
-        require(isDelayed, "Flight not delayed");
+        // Flight not delayed
+        if (!isDelayed) {
+            emit PayoutEvaluated(policyId, buyer, 0, PayoutResult.NotDelayed);
+            return (0, policy);
+        }
 
-        uint256 payout = delayHours * policy.template.payoutPerHour;
+        uint256 delayBeyondThreshold = delayHours > policy.template.delayThresholdHours
+            ? delayHours - policy.template.delayThresholdHours
+            : 0;
+
+        uint256 payout = delayBeyondThreshold * policy.template.payoutPerHour;
+
         if (payout > policy.template.maxTotalPayout) {
             payout = policy.template.maxTotalPayout;
         }
-        require(payout > 0, "No payout due");
+
+        // Flight delay is less than threshold
+        if (payout == 0) {
+            emit PayoutEvaluated(policyId, buyer, 0, PayoutResult.NoPayoutDue);
+            return (0, policy);
+        }
+
         policy.status = FlightPolicy.PolicyStatus.Claimed;
         policy.payoutToDate = payout;
+
+        emit PayoutEvaluated(policyId, buyer, payout, PayoutResult.Success);
         return (payout, policy);
     }
 
+    // ====== Internal Functions ======
     // Update the status of policies based on their expiry
     function updateStatus(UserPolicy[] memory policies, uint256 currentTime) internal pure returns (UserPolicy[] memory) {
         for (uint256 i=0; i < policies.length; i++) {
